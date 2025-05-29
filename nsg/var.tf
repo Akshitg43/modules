c775337b54220c731088e06f3cfce5cf7a474cca
Akshit <akshitg43@gmail.com>
2025-05-29 17:39:40 -0400

    new 99

---
+++ nsg/var.tf
@@ -0,0 +1,36 @@
+variable "nsg_name" {
+  description = "The name of the NSG"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource Group name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to the NSG"
+  type        = map(string)
+  default     = {}
+}
+
+variable "security_rules" {
+  description = "List of security rules"
+  type = list(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_port_range          = string
+    destination_port_range     = string
+    source_address_prefix      = string
+    destination_address_prefix = string
+  }))
+  default = []
+}
